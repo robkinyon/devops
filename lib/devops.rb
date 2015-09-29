b98b7b6955e2d58a4830450dc3b0c99a65f186ba
Rob Kinyon <rob.kinyon@gmail.com>
2015-09-29 15:50:47 -0400

    Add dns method to DevOps

--- lib/devops.rb
+++ lib/devops.rb
@@ -1,8 +1,19 @@
-require 'aws-sdk'
-
 require 'devops/error'
 
 require 'devops/dns'
 
+gem 'aws-sdk', '~> 2.1'
+require 'aws-sdk'
+
 class DevOps
+  def initialize(region='us-east-1')
+    Aws.config.update({
+      region: region,
+      credentials: Aws::SharedCredentials.new,
+    })
+  end
+
+  def dns
+    @dns ||= DevOps::DNS.new
+  end
 end
